5ee497ba4fbe0260e587ae794aae093dd675eb08
mraphael2101ba <mark.raphael@ba.com>
2023-05-26 18:01:50 +0100

    WIP

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,13 @@
 const { defineConfig } = require("cypress");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
 
-// defaultCommandTimeout: 5000, -> Overrides the default automation wait time globally
+async function setupNodeEvents(on, config) {
+  // implement node event listeners here which are loaded before every test run
+  await preprocessor.addCucumberPreprocessorPlugin(on, config)
+  on("file:preprocessor", browserify.default(config))
+  return config // return this object just in case it is modified by the plugin
+}
 
 module.exports = defineConfig({
 
@@ -15,10 +22,10 @@ module.exports = defineConfig({
   },
 
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents,
     specPattern: 'cypress/integration/examples/*/*.js',
     screenshotsFolder: 'cypress/failures/screenshots',
   },
 });
+
+// defaultCommandTimeout: 5000, -> Overrides the default automation wait time globally
