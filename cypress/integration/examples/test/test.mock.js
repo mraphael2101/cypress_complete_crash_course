1e3f3fe1210f528417d0347d4b30a72334b8be44
mraphael2101ba <mark.raphael@ba.com>
2023-06-13 17:20:58 +0100

    WIP

--- cypress/integration/examples/test/test.mock.js
+++ cypress/integration/examples/test/test.mock.js
@@ -9,23 +9,25 @@ describe("Mocking APIs Sample Test Suite", function () {
 
         // Request object:
         // Cypress will listen for this call to be made on the browser and intercept it
-        cy.intercept({
-            method: 'GET',
-            url: 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty'
-        },
+        cy.intercept(
+            {
+                method: 'GET',
+                url: 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty'
+            },
 
         // Response object:
         // Then you either send the real response or the mocked one. In this case we will return
         // the mocked one
-        {
-            statusCode: 200,
-            body: [
-                {
-                    "book_name": "RestAssured with Java",
-                    "isbn": "RSU",
-                    "aisle": "2301"
-                }
-            ]
+            {
+                statusCode: 200,
+                body:
+                    [
+                        {
+                            "book_name": "RestAssured with Java",
+                            "isbn": "RSU",
+                            "aisle": "2301"
+                        }
+                    ]
         }).as('bookretrievals')
 
         // Test Step 2
