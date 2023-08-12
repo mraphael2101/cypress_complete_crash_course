f413287841f6d6aaf9bb8644372d722d0c174d27
mraphael2101 <mark.raphael@ymail.com>
2023-08-12 22:49:58 +0100

    WIP

--- cypress/integration/examples/test/test.cy.readnested.file2.js
+++ cypress/integration/examples/test/test.cy.readnested.file2.js
@@ -1,27 +1,3 @@
-// const dir = '/Users/markraphael/Documents/workspace_javascript/cypress_bdd/cypress_complete_crash_course/cypress/fixtures/rewr';
-//
-// function readJsonFiles(filePath1, filePath2) {
-//     return cy.readFile(filePath1, 'utf8').then((json1) => {
-//         return cy.readFile(filePath2, 'utf8').then((json2) => {
-//             return {
-//                 json1: json1,
-//                 json2: json2
-//             };
-//         });
-//     });
-// }
-//
-// it('read and write out either a JSON object or the file contents as a string', () => {
-//     readJsonFiles(dir + '/archived.json', dir + '/derived.json')
-//         .then((results) => {
-//             const json1 = results.json1;
-//             const json2 = results.json2;
-//
-//             console.log('json1:', json1);
-//             console.log('json2:', json2);
-//         });
-// });
-
 const dir = '/Users/markraphael/Documents/workspace_javascript/cypress_bdd/cypress_complete_crash_course/cypress/fixtures/rewr';
 
 function readJsonFiles(filePath1, filePath2) {
--- cypress/integration/examples/test/test.cy.readwrite.file1.js
+++ cypress/integration/examples/test/test.cy.readwrite.file1.js
@@ -1,48 +1,8 @@
-// const dir = '/Users/markraphael/Documents/workspace_javascript/cypress_bdd/cypress_complete_crash_course/cypress/fixtures/rewr';
-//
-// /** cy.readFile reads a JSON file in Cypress and returns a string by default, not an object.
-//  * Only use JSON.parse on the result if it's an object, or it will cause an error.
-//  * If you pass an encoding argument of ‘utf8’ to cy.readFile, it will return a parsed JSON object */
-//
-// function readJsonFile(filePath) {
-//     // return cy.readFile(filePath).then((json) => {   // returns a string
-//     return cy.readFile(filePath, 'utf8').then((json) => {    // returns an object
-//         return json;
-//     }, (err) => {
-//         console.error(err);
-//     });
-// }
-//
-// function writeJsonFile(filePath, json) {
-//     return cy.writeFile(filePath, JSON.stringify(json)).then(() => {
-//     }, (err) => {
-//         console.error(err);
-//     });
-// }
-//
-// it('read and write out either a JSON object or the file contents as a string', () => {
-//     readJsonFile(dir + '/archived.json')
-//         .then((json) => {
-//             let obj;
-//             if (typeof json === 'object') {
-//                 obj = json;
-//             } else {
-//                 obj = JSON.parse(json);
-//             }
-//
-//             const newJson = { name: 'John', age: 30 };
-//
-//             cy.log(obj)
-//
-//             writeJsonFile(dir + '/derived.json', obj);
-//         });
-// });
-
-const dir = '/Users/markraphael/Documents/workspace_javascript/cypress_bdd/cypress_complete_crash_course/cypress/fixtures/rewr';
-
 /** cy.readFile reads a JSON file in Cypress and returns a string by default, not an object.
  * Only use JSON.parse on the result if it's an object, or it will cause an error.
  * If you pass an encoding argument of ‘utf8’ to cy.readFile, it will return a parsed JSON object */
+
+const dir = '/Users/markraphael/Documents/workspace_javascript/cypress_bdd/cypress_complete_crash_course/cypress/fixtures/rewr';
 
 function readJsonFile(filePath) {
     return cy.readFile(filePath, 'utf8').then((json) => {
@@ -68,7 +28,7 @@ it('read and write out either a JSON object or the file contents as a string', (
         .then((json) => {
             cy.log(json);
 
-            const newJson = { name: 'John', age: 30 };
+            const newJson = {name: 'John', age: 30};
 
             writeJsonFile(dir + '/derived.json', newJson);
         });
