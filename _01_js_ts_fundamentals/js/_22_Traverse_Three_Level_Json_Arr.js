3ced977b637cd0e587b2086108e994b6df6fa840
mraphael2101ba <mark.raphael@ba.com>
2023-08-23 12:18:59 +0100

    WIP

---
+++ _01_js_ts_fundamentals/js/_22_Traverse_Three_Level_Json_Arr.js
@@ -0,0 +1,40 @@
+var myArr = [
+    {
+        "planId": "somevalue1",
+        "parties": [
+            {
+                "attendees": [
+                    {
+                        "name": "anothervalue1",
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        "planId": "somevalue2",
+        "parties": [
+            {
+                "attendees": [
+                    {
+                        "name": "anothervalue2",
+                    }
+                ]
+            },
+        ]
+    }
+]
+
+
+for (let i = 0; i < myArr.length; i++) {
+    // Level 1
+    console.log(myArr[i].planId);
+    for (let j = 0; j < myArr[i].parties.length; j++) {
+        // Level 2
+        console.log(myArr[i].parties[j].attendees);
+        for (let k = 0; k < myArr[i].parties[j].attendees.length; k++) {
+            // Level 3
+            console.log(myArr[i].parties[j].attendees[k].name);
+        }
+    }
+}
