d379ec5e3ad3dabcfc21e383ac63fc36e770a4b4
mraphael2101ba <mark.raphael@ba.com>
2023-08-24 10:13:41 +0100

    WIP

--- _01_js_ts_fundamentals/js/_22_Traverse_Three_Level_Json_Arr.js
+++ _01_js_ts_fundamentals/js/_22_Traverse_Three_Level_Json_Arr.js
@@ -25,15 +25,30 @@ var myArr = [
     }
 ]
 
+// for (let i = 0; i < myArr.length; i++) {
+//     // Level 1
+//     console.log(myArr[i].planId);
+//     for (let j = 0; j < myArr[i].parties.length; j++) {
+//         // Level 2
+//         console.log(myArr[i].parties[j].attendees);
+//         for (let k = 0; k < myArr[i].parties[j].attendees.length; k++) {
+//             // Level 3
+//             console.log(myArr[i].parties[j].attendees[k].name);
+//         }
+//     }
+// }
+
+const planDetails = {};
+
 for (let i = 0; i < myArr.length; i++) {
-    // Level 1
-    console.log(myArr[i].planId);
-    for (let j = 0; j < myArr[i].parties.length; j++) {
-        // Level 2
-        console.log(myArr[i].parties[j].attendees);
-        for (let k = 0; k < myArr[i].parties[j].attendees.length; k++) {
-            // Level 3
-            console.log(myArr[i].parties[j].attendees[k].name);
-        }
-    }
+    const data = {
+        planId: myArr[i].planId,
+        name: myArr[i].parties[0].attendees[0].name
+    };
+
+    planDetails.planId = data.planId
+    planDetails.name = data.name
+    console.log(planDetails)
 }
+
+
