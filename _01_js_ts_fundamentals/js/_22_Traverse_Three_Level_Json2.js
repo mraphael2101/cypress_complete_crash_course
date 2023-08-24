dfbae4ddbcbef603f1846a96c0783d3337b19874
mraphael2101ba <mark.raphael@ba.com>
2023-08-24 10:27:41 +0100

    WIP

---
+++ _01_js_ts_fundamentals/js/_22_Traverse_Three_Level_Json2.js
@@ -0,0 +1,43 @@
+var body: {
+    "attendeeParties": [
+        {
+            "planId": "somevalue1",
+            "parties": [
+                {
+                    "attendees": [
+                        {
+                            "name": "anothervalue1",
+                        }
+                    ]
+                },
+            ]
+        },
+        {
+            "planId": "somevalue2",
+            "parties": [
+                {
+                    "attendees": [
+                        {
+                            "name": "anothervalue2",
+                        }
+                    ]
+                },
+            ]
+        }
+    ]
+}
+
+const planDetails = {};
+
+for (let i = 0; i < myArr.length; i++) {
+    const data = {
+        planId: myArr[i].planId,
+        name: myArr[i].parties[0].attendees[0].name
+    };
+
+    planDetails.planId = data.planId
+    planDetails.name = data.name
+    console.log(planDetails)
+}
+
+
