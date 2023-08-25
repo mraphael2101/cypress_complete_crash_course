d38e91004d899f84abea4c72b6698f5dd4d41489
mraphael2101 <mark.raphael@ymail.com>
2023-08-25 10:41:53 +0100

    WIP

---
+++ _01_js_ts_fundamentals/ts/demo2.ts
@@ -0,0 +1,56 @@
+function getPlanDetails(attendeeParties: Array<{
+    planId: string;
+    parties: Array<{
+        attendees: Array<{
+            name: string;
+        }>;
+    }>;
+}>): Array<{
+    planId: string;
+    names: string[];
+}> {
+    const planDetails = [];
+    for (const party of attendeeParties) {
+        const names = [];
+        for (const attendee of party.parties[0].attendees) {
+            names.push(attendee.name);
+        }
+        planDetails.push({
+            planId: party.planId,
+            names,
+        });
+    }
+    return planDetails;
+}
+
+const attendeeParties2 = [
+    {
+        planId: "somevalue1",
+        parties: [
+            {
+                attendees: [
+                    {
+                        name: "anothervalue1",
+                    },
+                    {
+                        name: "anothervalue2",
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        planId: "somevalue2",
+        parties: [
+            {
+                attendees: [
+                    {
+                        name: "anothervalue3",
+                    },
+                ],
+            },
+        ],
+    },
+];
+
+console.log(getPlanDetails(attendeeParties2));
